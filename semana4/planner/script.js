e0145b01135b7fec65ae990e9da2380ae9a85119
maxwelb-assis <max.assis@ymail.com>
2020-04-07 19:34:36 -0300

    planner concluido

--- semana4/planner/script.js
+++ semana4/planner/script.js
@@ -1,6 +1,35 @@
+ function apertarBotao() {
 
-function apertarBotao() {
-alert("teste")
-var botao = document.getElementById("testando")
-botao.innerHTML = "nao deu certo"
-}
+     let but = document.getElementById("entrada")
+     let segunda = document.getElementById("segunda")
+     let sele = document.getElementById("seletor")
+
+    if(sele.value == "segunda"){
+        segunda.innerHTML += but.value
+    }
+    else if (sele.value == "terca")  {
+        terca.innerHTML += but.value
+
+    }
+    else if (sele.value == "quarta")  {
+        quarta.innerHTML += but.value}
+
+    else if (sele.value == "quinta")  {
+        quinta.innerHTML += but.value}
+
+    else if (sele.value == "sexta")  {
+        sexta.innerHTML += but.value} 
+     
+     
+    else if (sele.value == "sabado")  {
+        sabado.innerHTML += but.value}
+
+    else {
+        domingo.innerHTML += but.value 
+    }
+    
+    but.value = ""
+     
+ 
+        
+ }
