b5cc8d55e55f9a1636fb2235f069d81330015d54
maxwelb-assis <max.assis@ymail.com>
2020-04-09 19:29:03 -0300

    melhorias no JS

--- semana4/funcoes-de-array/controlador-de-despesas/script.js
+++ semana4/funcoes-de-array/controlador-de-despesas/script.js
@@ -1,50 +1,66 @@
 let array = []
-let objeto = {} 
+let objeto = {}
+var despesatotal = []
+let maior = 0
+
 
 function apertar1() {
-   alert("test")
-   
+
     let input = document.getElementById("desp1")
-    let input2 = document.getElementById("desp2").value
+    let input2 = document.getElementById("desp2")
     let input3 = document.getElementById("desp3")
 
-    if (input.value === "" &&  input3.value ==="") {
+    if (input.value === "" && input3.value === "") {
         alert('Digite os Valores Corretamente P**** !!!!')
-    }
-    else {
+    } else {
 
-    objeto.valor = input.value
-    objeto.tipo = input2
-    objeto.descricao = input3.value
+        objeto.valor = input.value
+        objeto.tipo = input2.value
+        objeto.descricao = input3.value
 
-    console.log(objeto)
+        despesatotal = Number(despesatotal) + Number(input.value)
 
-    array += objeto
+        array += objeto
+        var despesas = document.getElementById("despesasfinal")
+        despesasfinal.innerHTML = despesatotal
 
-    console.log(array)
 
-    input.value = ""
-    input2.value = ""
-    input3.value =  ""
+        input.value = ""
+        input2.value = ""
+        input3.value = ""
+
     }
 }
 
+function apagar() {
+    let minimo = document.getElementById("min")
+    let maximo = document.getElementById("max")
+
+    minimo.value = ""
+    maximo.value = ""
+}
+
+
+
 
 /*function apertar2 () {
+alert('teste')
 
-    let input4 = document.getElementById("det").value
-    let input5 = document.getElementById("min").value
-    let input6 = document.getElementById("max").value
+let input4 = document.getElementById("det")
+let input5 = document.getElementById("min")
+let input6 = document.getElementById("max")
 
-    if (input5.value === "" &&  input6.value ==="") {
-        alert('Digite os Valores Corretamente P**** !!!!')
-    }
-    else {
+if (input5.value === "" &&  input6.value ==="") {
+    alert('Digite os Valores Corretamente P**** !!!!')
+}
+else {
+    
+    console.log(array)
+    
 
 
 
 
-
-
-    }
-}*/
+    
+    } 
+    }*/
