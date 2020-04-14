a4e86619e9344ae80df24395cd1b55c6107725a0
maxwelb-assis <max.assis@ymail.com>
2020-04-14 16:44:27 -0300

    exercicio componentes

---
+++ semana5/labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CardPequeno.css'
+
+
+export function CardPequeno(props) {
+return (
+    <div class="peq">
+{props.email}
+{props.end}
+
+
+</div>
+)
+}
+
+export default CardPequeno;
