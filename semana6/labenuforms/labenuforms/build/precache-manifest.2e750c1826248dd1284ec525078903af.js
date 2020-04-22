a3f461f92de91f6818b876809a2603e1ada92cbc
maxwelb-assis <max.assis@ymail.com>
2020-04-22 16:57:46 -0300

    exercicio lista-tarefas

---
+++ semana6/labenuforms/labenuforms/build/precache-manifest.2e750c1826248dd1284ec525078903af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea57326ebe7cbfa33685ef253e699722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f79b40f72d58195e0b4f",
+    "url": "/static/css/main.b96ba439.chunk.css"
+  },
+  {
+    "revision": "4ee5b826a900362c982d",
+    "url": "/static/js/2.c586addc.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.c586addc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79b40f72d58195e0b4f",
+    "url": "/static/js/main.bc728957.chunk.js"
+  },
+  {
+    "revision": "fc5971f5ee5d7d310955",
+    "url": "/static/js/runtime-main.a92696d9.js"
+  }
+]);
