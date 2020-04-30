77436ab219e8a57c1d9ebe028da8311e8b703b04
max <max@max.assis>
2020-04-30 16:58:58 -0300

    exercicio api

---
+++ semana7/api/meu-app/src/components/busca.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import style from "styled-components";
+import axios from "axios";
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Busca extends React.Component {
+  state={
+
+
+
+
+    
+  }
+  
+  
+  
+  
+    render() {
+    return (
+      <div>
+        <h1>testando o react</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Busca;
