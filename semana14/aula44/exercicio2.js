42647417bdeb80dffcf33ec367da5ebe02a56e5d
maxwelb-assis <max.assis@ymail.com>
2020-06-22 17:19:01 -0300

    exercicios typescript

---
+++ semana14/aula44/exercicio2.js
@@ -0,0 +1,4 @@
+var calculo = function (num1, num2) {
+    num2 > num1 ? console.log(num2 + " e maior") : console.log(num1 + " e maior");
+};
+calculo(2, 8);
---
+++ semana14/aula44/exercicio2.ts
@@ -0,0 +1,6 @@
+var calculo = (num1: number, num2: number) => {
+    num2 > num1? console.log(`${num2} e maior`) : console.log(`${num1} e maior`)
+    
+} 
+
+calculo(2, 8)
