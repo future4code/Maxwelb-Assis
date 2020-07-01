99654036490a70973f3bd6b32e3bd6671b25b23e
maxwelb-assis <max.assis@ymail.com>
2020-06-30 22:37:20 -0300

    exercicio 49

---
+++ semana15/aula49/src/index.ts
@@ -0,0 +1,69 @@
+// exercicio1 
+
+class User {
+  private id: string;
+  private email: string;
+  private name: string;
+  private password: string;
+
+  constructor(
+		id: string,
+		email: string,
+		name: string,
+		password: string
+	){
+		console.log("Chamando o construtor da classe User")
+		this.id = id
+		this.email = email
+		this.name = name 
+		this.password = password
+	}
+
+	public getId(): string {
+		return this.id
+	}
+
+	public getEmail(): string {
+        return this.email
+	}
+
+	public getName(): string {
+		return this.name
+    }
+   
+}
+
+// a)nao pois esta privado
+// b) nenhuma vez
+
+
+// exercicio 2
+
+class Customer extends User {
+    public purchaseTotal: number = 0;
+    private creditCard: string;
+  
+    constructor(
+      id: string,
+      email: string,
+      name: string,
+      password: string,
+      creditCard: string
+    ) {
+      super(id, email, name, password);
+      console.log("Chamando o construtor da classe Customer");
+      this.creditCard = creditCard;
+    }
+  
+    public getCreditCard(): string {
+      return this.creditCard;
+    }
+  }
+
+  // nenhuma vez
+  
+ // exercicio 3
+
+ 
+
+
